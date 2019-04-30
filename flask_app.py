e28eb6c134a964e87087928ddda0d14e30c55fd0
m4m6o <45889084+m4m6o@users.noreply.github.com>
2019-04-30 11:56:52 +0600

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,17 +1,12 @@
 from flask import Flask, request
 import logging
 import json
-# импортируем функции из нашего второго файла geo
 from geo import get_country, get_distance, get_coordinates
  
 app = Flask(__name__)
- 
-# Добавляем логирование в файл. Чтобы найти файл, 
-# перейдите на pythonwhere в раздел files, он лежит в корневой папке
 logging.basicConfig(level=logging.INFO, filename='app.log',
                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
- 
- 
+  
 @app.route('/post', methods=['POST'])
 def main():
     logging.info('Request: %r', request.json)
@@ -31,22 +26,21 @@ def handle_dialog(res, req):
     user_id = req['session']['user_id']
     if req['session']['new']:
         res['response']['text'] = \
-            'Привет! Я могу показать город или сказать расстояние между городами!'
+            'ГЏГ°ГЁГўГҐГІ! Гџ Г¬Г®ГЈГі ГЇГ®ГЄГ Г§Г ГІГј ГЈГ®Г°Г®Г¤ ГЁГ«ГЁ Г±ГЄГ Г§Г ГІГј Г°Г Г±Г±ГІГ®ГїГ­ГЁГҐ Г¬ГҐГ¦Г¤Гі ГЈГ®Г°Г®Г¤Г Г¬ГЁ!'
         return
-    # Получаем города из нашего
     cities = get_cities(req)
     if not cities:
-        res['response']['text'] = 'Ты не написал название не одного города!'
+        res['response']['text'] = 'Г’Г» Г­ГҐ Г­Г ГЇГЁГ±Г Г« Г­Г Г§ГўГ Г­ГЁГҐ Г­ГҐ Г®Г¤Г­Г®ГЈГ® ГЈГ®Г°Г®Г¤Г !'
     elif len(cities) == 1:
-        res['response']['text'] = 'Этот город в стране - ' + \
+        res['response']['text'] = 'ГќГІГ®ГІ ГЈГ®Г°Г®Г¤ Гў Г±ГІГ°Г Г­ГҐ - ' + \
             get_country(cities[0])
     elif len(cities) == 2:
         distance = get_distance(get_coordinates(
             cities[0]), get_coordinates(cities[1]))
-        res['response']['text'] = 'Расстояние между этими городами: ' + \
-            str(round(distance)) + ' км.'
+        res['response']['text'] = 'ГђГ Г±Г±ГІГ®ГїГ­ГЁГҐ Г¬ГҐГ¦Г¤Гі ГЅГІГЁГ¬ГЁ ГЈГ®Г°Г®Г¤Г Г¬ГЁ: ' + \
+            str(round(distance)) + ' ГЄГ¬.'
     else:
-        res['response']['text'] = 'Слишком много городов!'
+        res['response']['text'] = 'Г‘Г«ГЁГёГЄГ®Г¬ Г¬Г­Г®ГЈГ® ГЈГ®Г°Г®Г¤Г®Гў!'
  
  
 def get_cities(req):
